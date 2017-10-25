a18b7f320369e579853db71e1ae79225b19f3f10
yixue-shuguo <32661917+yixue-shuguo@users.noreply.github.com>
2017-10-25 10:41:36 +0800

    20171025
    开始写自动报表

---
+++ temp_demannds/课前课后回访情况.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 10:28:51 2017
+
+@author: Administrator
+"""
+
+from all_common_packages.DB.connect_mysql import mysql
+
+host = 'rr-uf647q511648367cso.mysql.rds.aliyuncs.com'
+user = 'crm_root'
+password = 'Hello2017'
+database = 'ecustomer'
+
+conn = mysql(host,user,password ,database)
+
